41df4a463024a9a00afea21a88429598d926b807
Nicolas <nicolas.silva@utec.edu.uy>
2024-06-04 17:57:10 -0300

    cambio a la barra por el btn de login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { PerfilComponent } from './perfil/perfil.component';
 import { PokedexComponent } from './pokedex/pokedex.component';
 import { TeamComponent } from './team/team.component';
 import { BarraComponent } from './barra/barra.component';
-
+import { LoginComponent } from './login/login.component';
 
 
 
@@ -14,5 +14,6 @@ export const routes: Routes = [
   { path: 'perfil', component: PerfilComponent},
   { path: 'pokedex', component: PokedexComponent},
   { path: 'team', component: TeamComponent},
+  { path: 'login', component: LoginComponent},
   { path: '**', redirectTo:'inicio', pathMatch:'full'}
 ];
