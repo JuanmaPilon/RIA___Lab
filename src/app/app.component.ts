2b99b312ff0edf7c2ea8f0d36707c29a1d86fd83
Juanma <juanmartin.pilon@gmail.com>
2024-06-04 17:31:28 -0300

    agregandole el footer arriba de los cambiosa que hicimos con el negro,carlangas y el kuro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { BarraComponent } from './barra/barra.component';
 import { CommonModule } from '@angular/common';
 import { PokedexComponent } from './pokedex/pokedex.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BarraComponent, CommonModule, PokedexComponent],
+  imports: [RouterOutlet, BarraComponent, CommonModule, PokedexComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
