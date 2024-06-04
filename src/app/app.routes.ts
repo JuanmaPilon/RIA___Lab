513ab48ffae7ba8225b10737e59ac81ec8d826f8
Nicolas <nicolas.silva@utec.edu.uy>
2024-06-04 01:04:13 -0300

    sumo configs
    PD: lleven tranquilos que no rompe

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { PerfilComponent } from './perfil/perfil.component';
+import { PokedexComponent } from './pokedex/pokedex.component';
+import { TeamComponent } from './team/team.component';
+import { BarraComponent } from './barra/barra.component';
 
-export const routes: Routes = [];
+
+
+
+export const routes: Routes = [
+  { path: 'barra', component: BarraComponent},
+  { path: 'inicio', component: InicioComponent},
+  { path: 'perfil', component: PerfilComponent},
+  { path: 'pokedex', component: PokedexComponent},
+  { path: 'team', component: TeamComponent},
+  { path: '**', redirectTo:'inicio', pathMatch:'full'}
+];
---
+++ src/app/barra/barra.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-barra',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './barra.component.html',
+  styleUrl: './barra.component.css'
+})
+export class BarraComponent {
+
+}
