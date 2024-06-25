6c297591295cddccaaf228cdb6946544b828f96c
Juanma <juanmartin.pilon@gmail.com>
2024-06-25 14:53:42 -0300

    Trabajando en el inicio componente, para la landing page, ta tirando error con el carrusel pero queda ver eso

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BarraComponent } from './barra/barra.component';
-import { CommonModule } from '@angular/common';
 import { PokedexComponent } from './pokedex/pokedex.component';
 import { FooterComponent } from './footer/footer.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BarraComponent, CommonModule, PokedexComponent, FooterComponent],
+  imports: [RouterOutlet, BarraComponent, PokedexComponent, FooterComponent, InicioComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,12 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PokeapiService } from './../pokeapi.service';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [],
+  imports: [CarouselModule, CommonModule],
   templateUrl: './inicio.component.html',
-  styleUrl: './inicio.component.css'
+  styleUrls: ['./inicio.component.css']
 })
-export class InicioComponent {
+export class InicioComponent implements OnInit {
+  pokemonImages: string[] = [];
+  customOptions: any = {
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 3
+      },
+      1000: {
+        items: 5
+      }
+    }
+  };
 
+  constructor(private pokeapiService: PokeapiService) { }
+
+  ngOnInit(): void {
+    this.loadRandomPokemonImages();
+  }
+
+  loadRandomPokemonImages(): void {
+    this.pokeapiService.getAllPokemons().subscribe({
+      next: (pokemons) => {
+        const randomPokemons = this.getRandomItems(pokemons, 5);
+        this.pokemonImages = randomPokemons.map(pokemon => pokemon.imageUrl);
+      },
+      error: (error) => {
+        console.error('Error al cargar los Pokémon:', error);
+      }
+    });
+  }
+
+  getRandomItems(arr: any[], count: number): any[] {
+    const shuffled = arr.slice(0);
+    let i = arr.length;
+    let temp, index;
+    while (i--) {
+      index = Math.floor(Math.random() * (i + 1));
+      temp = shuffled[index];
+      shuffled[index] = shuffled[i];
+      shuffled[i] = temp;
+    }
+    return shuffled.slice(0, count);
+  }
 }
